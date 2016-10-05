6605122bc9b51261415c59faaa3104903c581f6e
Daniela Dias <daniela@ustwo.com>
2016-10-05 16:13:08 +0100

    Initial commit.

---
+++ VIPDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VIPDemo
+//
+//  Created by Daniela Dias on 05/10/2016.
+//  Copyright © 2016 ustwo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
