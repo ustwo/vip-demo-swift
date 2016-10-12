c4f7ddfdcbcab655c0db5adfd0844859f78fd969
Daniela Dias <daniela@ustwo.com>
2016-10-12 12:57:53 +0100

    Album and Artist model unit tests added.

--- VIPDemoTests/Shared/AlbumTests.swift
+++ VIPDemoTests/Shared/AlbumTests.swift
@@ -9,6 +9,9 @@
 import XCTest
 @testable import VIPDemo
 
+
+// MARK: - AlbumTests
+
 final class AlbumTests: XCTestCase {
 
 
@@ -16,5 +19,37 @@ final class AlbumTests: XCTestCase {
 
     func testFromJSONReturnsAlbum() {
 
+        // Given
+
+        let bundle = Bundle(for: AlbumTests.self)
+        let path = bundle.path(forResource: "album", ofType: "json")!
+        let url = URL(fileURLWithPath: path)
+
+        do {
+
+            let data = try Data(contentsOf: url, options: Data.ReadingOptions.mappedIfSafe)
+
+            if let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] {
+
+                // When
+
+                let album = Album.fromJSON(json: json)
+
+                // Then
+
+                XCTAssertEqual(album?.mbid, "219b202d-290e-3960-b626-bf852a63bc50")
+                XCTAssertEqual(album?.name, "A Rush of Blood to the Head")
+                XCTAssertEqual(album?.url.absoluteString, "https://www.last.fm/music/Coldplay/A+Rush+of+Blood+to+the+Head")
+                XCTAssertEqual(album?.imageURL?.absoluteString, "https://lastfm-img2.akamaized.net/i/u/174s/84d00c7ea1ec48ebc84cabae1bad75dc.png")
+
+            } else {
+
+                XCTFail()
+            }
+
+        } catch {
+
+            XCTFail()
+        }
     }
 }
--- VIPDemoTests/Shared/ArtistTests.swift
+++ VIPDemoTests/Shared/ArtistTests.swift
@@ -9,6 +9,9 @@
 import XCTest
 @testable import VIPDemo
 
+
+// MARK: - ArtistTests
+
 final class ArtistTests: XCTestCase {
 
 
@@ -16,5 +19,39 @@ final class ArtistTests: XCTestCase {
 
     func testFromJSONReturnsArtist() {
 
+        // Given
+
+        let bundle = Bundle(for: ArtistTests.self)
+        let path = bundle.path(forResource: "artist", ofType: "json")!
+        let url = URL(fileURLWithPath: path)
+
+        do {
+
+            let data = try Data(contentsOf: url, options: Data.ReadingOptions.mappedIfSafe)
+
+            if let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] {
+
+                // When
+
+                let artist = Artist.fromJSON(json: json)
+
+                // Then
+
+                XCTAssertEqual(artist?.mbid, "cc197bad-dc9c-440d-a5b5-d52ba2e14234")
+                XCTAssertEqual(artist?.name, "Coldplay")
+                XCTAssertEqual(artist?.playCount, "334356991")
+                XCTAssertEqual(artist?.listeners, "5126338")
+                XCTAssertEqual(artist?.url.absoluteString, "https://www.last.fm/music/Coldplay")
+                XCTAssertEqual(artist?.imageURL?.absoluteString, "https://lastfm-img2.akamaized.net/i/u/300x300/0c057486667d4ec8ce07e1b964bce20c.png")
+
+            } else {
+
+                XCTFail()
+            }
+
+        } catch {
+
+            XCTFail()
+        }
     }
 }
