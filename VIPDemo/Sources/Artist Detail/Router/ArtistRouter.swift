54353a0a64712c498d4546eabe129583ba9446e2
Daniela Dias <daniela@ustwo.com>
2016-10-13 15:13:24 +0100

    Hidden view controller in protocol from being set outside.

--- VIPDemo/Sources/Artist Detail/Router/ArtistRouter.swift
+++ VIPDemo/Sources/Artist Detail/Router/ArtistRouter.swift
@@ -14,7 +14,7 @@ import UIKit
 /// _ArtistRouterProtocol_ is a protocol for router input behaviours
 protocol ArtistRouterProtocol {
 
-    weak var viewController: ArtistViewController? { get set }
+    weak var viewController: ArtistViewController? { get }
 }
 
 
